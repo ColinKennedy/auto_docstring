993add6473d3e9993a59ebb2ffd5fd4658fb9953
Colin Kennedy <colinvfx@gmail.com>
2018-03-05 08:22:39 -0500

    args_block.py - Refactored string into a function

--- pythonx/auto_docstring/blocks/google/args_block.py
+++ pythonx/auto_docstring/blocks/google/args_block.py
@@ -13,6 +13,15 @@ class Args(common_block.CommonBlock):
     label = 'Args'
     name = 'args'
 
+    @staticmethod
+    def _make_line(arg, number=0, value=''):
+        indent = common.get_default_indent()
+        if value:
+            return '{indent}{arg} ({{{id_}|{value}}}, optional): {{}}.'.format(
+                indent=indent, arg=arg, id_=number, value=value)
+        else:
+            return '{indent}{arg} ({{}}): {{}}.'.format(indent=indent, arg=arg)
+
     @classmethod
     def draw(cls, info):
         args = info.get('args', [])
@@ -23,12 +32,11 @@ class Args(common_block.CommonBlock):
         if not args and not defaults and not vararg:
             return []
 
-        indent = common.get_default_indent()
         starting_line = '{}:'.format(cls.label)
         lines = [starting_line]
 
         for arg in args:
-            line = '{indent}{arg} ({{}}): {{}}.'.format(indent=indent, arg=arg)
+            line = cls._make_line(arg)
             lines.append(line)
 
         for index, (arg, value) in enumerate(defaults):
@@ -37,31 +45,14 @@ class Args(common_block.CommonBlock):
             else:
                 value = common_block.get_type_name(visit.get_value(value))
 
-            line = '{indent}{arg} ({{{id_}|{value}}}, optional): {{}}.'.format(
-                indent=indent,
-                arg=arg,
-                id_=index,
-                value=value,
-            )
+            line = cls._make_line(arg=arg, number=index, value=value)
             lines.append(line)
 
         if vararg:
-            lines.append(
-                '{indent}{arg} ({{{id_}|{value}}}, optional): {{}}.'.format(
-                    indent=indent,
-                    arg='*' + vararg,
-                    id_=len(defaults),
-                    value='tuple',
-            ))
+            lines.append(cls._make_line(arg='*' + vararg, number=len(defaults), value='tuple'))
 
         if kwarg:
-            lines.append(
-                '{indent}{arg} ({{{id_}|{value}}}, optional): {{}}.'.format(
-                    indent=indent,
-                    arg='**' + kwarg,
-                    id_=len(defaults) + 1,
-                    value='dict',
-            ))
+            lines.append(cls._make_line(arg='**' + kwarg, number=len(defaults) + 1, value='dict'))
 
         return lines
 
