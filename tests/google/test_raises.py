e50ab2ba834bd97d2e5e83c30da2f972540a73fe
Colin Kennedy <colinvfx@gmail.com>
2018-03-10 20:29:03 -0500

    Added another unittest for test_raises.py for a bug concerning strings that contain {}s

--- tests/google/test_raises.py
+++ tests/google/test_raises.py
@@ -5,6 +5,54 @@
 from .. import common
 
 
+class GenericTestCase(common.CommonTestCase):
+    def test_002(self):
+        # Test that a message that starts with {, another that ends in }, and
+        # one more that does both
+        #
+        code = self._make_code(
+            '''
+            def foo(arg1, arg2, thing=(('asfd', 'asdfsfd'), )):
+                %s
+                message = 'asdfsd'
+                if arg2:
+                    raise ValueError(message)
+
+                if thing[0]:
+                    raise TypeError('{ffff}bar'.format(ffff=9123))
+
+                if thing[1]:
+                    raise RuntimeError('{zzzz}'.format(zzzz=9123))
+
+                if arg1:
+                    raise NotImplementedError('bar{tttt}'.format(tttt=9123))
+
+                return ['asdfsdf', 'adsfafds']
+            ''')
+
+        expected_output = \
+            '''\
+            {1!f}.
+
+            Args:
+                arg1 ({2!f}): {3!f}.
+                arg2 ({4!f}): {5!f}.
+                thing ({6:tuple[tuple[str]]!f}, optional): {7!f}.
+
+            Raises:
+                ValueError: {8:asdfsd!f}.
+                TypeError: {10:{9:ffff!f}bar!f}.
+                RuntimeError: {12:{11:zzzz!f}!f}.
+                NotImplementedError: {14:bar{13:tttt!f}!f}.
+
+            Returns:
+                {15:list[str]!f}: {16!f}.
+
+            '''
+
+        self.compare(expected_output, code)
+
+
 class NameTestCase(common.CommonTestCase):
     def test_variable_message(self):
         code = \
