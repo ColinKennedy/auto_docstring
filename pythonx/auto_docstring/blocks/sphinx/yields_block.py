38c3d7bbd960cfacfbe2c7652828476a77c9adb7
Colin Kennedy <colinvfx@gmail.com>
2018-03-24 09:41:10 -0400

    sphinx/yields_block.py - Added 'yields' key

--- pythonx/auto_docstring/blocks/sphinx/yields_block.py
+++ pythonx/auto_docstring/blocks/sphinx/yields_block.py
@@ -4,9 +4,12 @@
 '''The class and functions needed to print a Sphinx-style "Yields" block.'''
 
 # IMPORT LOCAL LIBRARIES
-from .sphinx import returns_block
+from . import returns_block
 
 
 class Yields(returns_block.Returns):
 
     '''The "Yields" block main class.'''
+
+    name = 'yields'
+    _info_key = name
